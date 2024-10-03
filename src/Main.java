10daab8d09eced02c25895a479f8c48e8fea33b1
Anna Nikiforova <nikiforova@mad-soft.ru>
2024-10-03 14:53:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Tasha", -8);
+        cat1.setName("Tarrr");
+        //cat1.setAge(-6);
+
+
+        System.out.println(cat1.getName());
+        System.out.println(cat1.getAge());
+
+    }
+
+}
+class Cat{
+    private String name;
+    private int age;
+
+    public Cat(String name, int age){
+        this.name = name;
+        if(age >= 0){
+            this.age = age;
+        } else {
+            System.out.println("error");
+        }
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        if(age >= 0){
+            this.age = age;
+        } else {
+            System.out.println("error! we cant");
+        }
+    }
+}
+
+
